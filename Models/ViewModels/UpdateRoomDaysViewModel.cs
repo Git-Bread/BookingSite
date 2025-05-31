22b0c0f759c9671f11581e07de1c33485135f137
Max Gagner <m.gagner@hotmail.se>
2025-05-31 19:39:01 +0200

    small fixes

--- Models/ViewModels/UpdateRoomDaysViewModel.cs
+++ Models/ViewModels/UpdateRoomDaysViewModel.cs
@@ -3,6 +3,6 @@ namespace BookingSite.Models.ViewModels
     public class UpdateRoomDaysViewModel
     {
         public int RoomId { get; set; }
-        public int[] OpenDays { get; set; }
+        public int[] OpenDays { get; set; } = Array.Empty<int>();
     }
 } 
